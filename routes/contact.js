aa4b0774de4d808babece15096eeb0c98ca0fcfe
csc530 <cscousin@lakeheadu.ca>
2022-02-14 18:56:39 -0500

    chore(main): :alembic: secure email val test

--- routes/contact.js
+++ routes/contact.js
@@ -23,7 +23,7 @@ router.post('/', function (req, res, next) {
 		host: 'smtp.gmail.com',
 		port: 587,
 		//change to production URL
-		secure: 'https://csc530-portfolio.herokuapp.com/',
+		secure: true,
 		requireTLS: true,
 		auth: {
 			type: 'LOGIN',
