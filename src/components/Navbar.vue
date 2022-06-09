d74fc55cd40caa857772fe330a2e138b744dc0e9
csc530 <cscousin@lakeheadu.ca>
2022-06-09 10:28:02 -0400

    chore(pages): :truck: Change name of views dir

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+	<nav>
+		<router-link to="/" class="nav-link">Home</router-link>
+		<router-link to="/about" class="nav-link">About</router-link>
+	</nav>
+</template>
+
+<script setup>
+	import {ref} from 'vue';
+	
+	const Hi = ref('Hello World!');
+</script>
+
+<style scoped></style>
