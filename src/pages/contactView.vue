d705c0226e00ea3a682bbd84b9d07a0c542da31a
csc530 <cscousin@lakeheadu.ca>
2022-06-21 20:38:53 -0400

    :lipstick: feat(contact me): created contact page
    Made form for contact me page, no functionality

---
+++ src/pages/contactView.vue
@@ -0,0 +1,20 @@
+<template>
+	<h1>Hit me up!</h1>
+	<form accept-charset="UTF-8" method="post" action="" enctype="text/plain" name="contactForm">
+		<label class="label" for="from">From:
+			<input type="text" name="from" id="from" required class="input" /></label>
+		<label for="subject" class="label">Subject:
+			<input type="text" name="subject" class="input" id="subject" required /></label>
+		<label for="message" class="label">Message:
+			<textarea name="message" id="message" required class="textarea"></textarea></label>
+		<input type="submit" value="Send" class="input button" />
+	</form>
+</template>
+
+<script setup>
+// TODO: look up how to use forms in Vue (https://vuejs.org/guide/essentials/forms.html)
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,29 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../pages/HomeView.vue";
 
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
+let homeRoute = {
+  path: "/",
+  name: "home",
+  component: HomeView,
+};
+const aboutRoute = {
+  path: "/about",
+  name: "about",
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () =>
       import(/* webpackChunkName: "about" */ "../pages/AboutView.vue"),
-  },
+};
+const contactRoute = {
+  path: '/contact',
+  name: 'contact',
+  component: () => import('../pages/contactView.vue'),
+};
+const routes: Array<RouteRecordRaw> = [
+  homeRoute,
+  aboutRoute,
+  contactRoute
 ];
 
 const router = createRouter({
