8a4cfa31572a8119f75ef741641deb6b55e241b4
csc530 <cscousin@lakeheadu.ca>
2022-06-25 15:34:46 +0200

    :zap: perf(main): Improved props extrapolation
    Made the extraction and title more forgiving to non-uniform objects
    
    Signed-off-by: csc530 <cscousin@lakeheadu.ca>

--- src/components/HeroHome.vue
+++ src/components/HeroHome.vue
@@ -1,11 +1,9 @@
 <template>
 	<section class="hero is-success is-fullheight">
 		<!-- Hero head: will stick at the top -->
-		<header class="navbar hero-head">
+		<header class="navbar hero-head ml-1 mr-1">
 			<div class="navbar-brand">
-				<a class="navbar-item">
-					<img alt="Logo" src="https://bulma.io/images/bulma-type-white.png">
-				</a>
+				<p class="title"><strong>Christofer Cousins</strong></p>
 				
 				<div class="navbar-burger" data-target="navbarMenuHeroC">
 					<span></span>
@@ -15,54 +13,83 @@
 			</div>
 			
 			<nav id="navbarMenuHeroC" class="navbar-menu navbar-end">
-				<router-link v-for="router in $router.getRoutes()" :to="router.path" active-class="is-active"
+				<router-link v-for="router in $router.getRoutes()" :to="router.path" active-class="has-text-weight-bold"
 				             class="navbar-item">
 					{{ router.name }}
 				</router-link>
-				
-				<span class="navbar-item">
-					<a class="button is-success is-inverted">
-						<span class="icon">
-							<i class="fab fa-github"></i>
-						</span>
-							<span>Download</span>
-					</a>
-				</span>
 			</nav>
 		</header>
 		
 		<!-- Hero content: will be in the middle -->
 		<main class="hero-body container has-text-centered is-flex-wrap-wrap is-flex-direction-column">
-			<h1 class="title">{{ topics[currentTopic].name }}</h1>
-			<!--			<h2 class="subtitle">{{ main.subtitle }}</h2>-->
+			<h1 class="title">{{ getTopicTitle(topics[currentTopic]) }}</h1>
 			<p>
-				<slot>{{ topics[currentTopic].value }}</slot>
+				<slot>{{ getTopicTxt(topics[currentTopic]) }}</slot>
 			</p>
 		</main>
 		
 		<!-- Hero footer: will stick at the bottom -->
 		<footer class="hero-foot">
 			<ul class="tabs is-boxed is-fullwidth container m-auto has-text-centered">
-				<li v-for="(topic, index) in topics" @click="display(index)">{{ topic.name }}</li>
+				<li v-for="(topic, index) in topics" :class="activeTopic(index)" @click="display(index)">{{
+						getTopicTitle(topic)
+					}}
+				</li>
 			</ul>
 		</footer>
 	</section>
 </template>
 
-<script lang="ts" setup>
-import {defineProps, ref} from "vue";
-
-const props = defineProps({
-	topics: {
-		type: Array,
-		required: true,
-		default: () => []
+<style scoped>
+	
+	.hero-foot li:hover {
+		cursor          : pointer;
+		backdrop-filter : invert(100%);
 	}
-});
-const currentTopic = ref(0);
-const display = (index: number) => {
-	console.log(index);
-	currentTopic.value = index;
-};
+</style>
+
+<script lang="ts" setup>
+	import {ref} from "vue";
+	
+	const props = defineProps({
+		topics: {
+			type: Array,
+			required: true
+		}
+	});
+	
+	const currentTopic = ref(0);
+	const display = (index: number) => {
+		console.log(index);
+		currentTopic.value = index;
+	};
+	
+	function activeTopic(index: number) {
+		//TODO: Add dim to background and border of active tab
+		return index === currentTopic.value ? 'has-text-weight-bold' : '';
+	}
+	
+	function getTopicTitle(topic: any) {
+		console.dir(topic);
+		if(typeof (topic) !== 'object')
+			return;
+		const {title, name} = topic;
+		if(title)
+			return title;
+		else if(name)
+			return name;
+		else
+			return Object.values(topic)[0];
+	}
+	
+	function getTopicTxt(topic: any) {
+		if(typeof (topic) !== 'object')
+			return;
+		const {txt} = topic;
+		if(txt)
+			return txt;
+		else
+			return Object.values(topic)[1];
+	}
 
 </script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -11,17 +11,41 @@ export default class Home extends Vue {
 	///todo: maybe add firebase access to load txt for all the topics of me
 	topics = [
 		{
-			name: 'Overview',
-			path: '/'
+			title: 'Christofer Cousins',
+			txt: `Hey I'm Christofer Cousins, I am a software developer and I love to code. I am currently working on a project called "The-Maze".`
 		},
 		{
-			name: 'About',
-			path: '/about'
+			title: 'Student',
+			txt: 'Lakehead Georgian Honours Bachelor of Science (Computer Science)',
+			img: 'lakehead-georgian.png',
+			link: '/about'
 		},
 		{
-			name: 'Contact',
-			path: '/contact'
-		}
+			title: 'Aspiring Full-Stack Developer',
+			txt: 'I currently have skills in web and desktop application development. I am working on my android applications and data structures.',
+			img: 'programmer-typing.gif',
+			link: '/about'
+		},
+		{
+			title: 'Projects',
+			txt: 'more project work available upon request',
+			items: [
+				{name: 'Crypto.com Exchange API - JavaScript', link: 'https://github.com/csc530/crypto.com-exhcange-API'},
+				{
+					name: "Nasa's Astronmy picture of the Day application - Java",
+					link: 'https://github.com/csc530/advOOP-assignment2'
+				},
+				{name: 'Content managment website - PHP', link: 'https://github.com/csc530/website-maker'},
+				{name: 'Family tree app - Kotlin', link: 'https://csc530.github.io/Family-Tree/'},
+			],
+			img: 'project.jpg',
+		},
+		{
+			title: 'A picture of me😃',
+			alt: 'A photo of me',
+			src: 'portrait.jpg',
+			link: '/about'
+		},
 	];
 }
 </script>
