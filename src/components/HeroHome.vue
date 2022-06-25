4c6fee077d79e3c8544d07ec8909a91ca368aa89
csc530 <cscousin@lakeheadu.ca>
2022-06-24 20:52:12 -0400

    :sparkles: feat(index): Created page
    Made a full page for the home page; a Bulma hero
    
    Signed-off-by: csc530 <cscousin@lakeheadu.ca>

---
+++ src/components/HeroHome.vue
@@ -0,0 +1,62 @@
+<template>
+	<section class="hero is-success is-fullheight">
+		<!-- Hero head: will stick at the top -->
+		<header class="navbar hero-head">
+			<div class="navbar-brand">
+				<a class="navbar-item">
+					<img alt="Logo" src="https://bulma.io/images/bulma-type-white.png">
+				</a>
+				<div class="navbar-burger" data-target="navbarMenuHeroC">
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+			</div>
+			<nav id="navbarMenuHeroC" class="navbar-menu navbar-end">
+				<a class="navbar-item is-active">Home</a>
+				<a class="navbar-item">Examples</a>
+				<a class="navbar-item">Documentation</a>
+				<span class="navbar-item">
+					<a class="button is-success is-inverted">
+						<span class="icon">
+							<i class="fab fa-github"></i>
+						</span>
+							<span>Download</span>
+					</a>
+				</span>
+			</nav>
+		</header>
+		
+		<!-- Hero content: will be in the middle -->
+		<main class="hero-body container has-text-centered is-flex-wrap-wrap is-flex-direction-column">
+			<h1 class="title">{{ title }}</h1>
+			<h2 class="subtitle">{{ subtitle }}</h2>
+			<p>
+				<slot>{{ txt }}</slot>
+			</p>
+		</main>
+		
+		<!-- Hero footer: will stick at the bottom -->
+		<footer class="hero-foot">
+			<ul class="tabs is-boxed is-fullwidth container m-auto">
+				<li><a>Overview</a></li>
+				<li><a>Modifiers</a></li>
+				<li><a>Grid</a></li>
+				<li><a>Elements</a></li>
+				<li><a>Components</a></li>
+				<li><a>Layout</a></li>
+			</ul>
+		</footer>
+	</section>
+</template>
+
+<script lang="ts" setup>
+import {defineProps} from "vue";
+
+const props = defineProps({
+	title: String,
+	subtitle: String,
+	txt: String
+});
+
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+	<hero-home></hero-home>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import {Options, Vue} from "vue-class-component";
+import HeroHome from '@/components/HeroHome.vue';
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+@Options({components: {HeroHome}})
+export default class Home extends Vue {
+}
 </script>
