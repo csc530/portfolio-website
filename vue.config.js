76131b2a7ff392708bde40bb2383497464085150
csc530 <cscousin@lakeheadu.ca>
2022-07-28 17:42:33 +0200

    :bento: chore(assets): Add assets to project
    Signed-off-by: csc530 <cscousin@lakeheadu.ca>

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,16 @@
-const { defineConfig } = require("@vue/cli-service");
+const {defineConfig} = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
-
-  css: {
-    extract: true
-  },
-
-  pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'sass',
-      patterns: [
-          './styles/*.sass'
-      ]
-    }
-  }
+	transpileDependencies: true,
+	assetsDir: "assets",
+	css: {
+		extract: true
+	},
+	pluginOptions: {
+		'style-resources-loader': {
+			preProcessor: 'sass',
+			patterns: [
+				'./styles/*.sass'
+			]
+		}
+	}
 });
