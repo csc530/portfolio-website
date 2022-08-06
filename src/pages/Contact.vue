8057a3beee596a8d8cffe7889168b159b8e46817
csc530 <cscousin@lakeheadu.ca>
2022-08-06 10:37:57 -0400

    feat(pages): :construction: add response to email send

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,6 +1,6 @@
 <template>
 	<h1>Hit me up!</h1>
-	<form accept-charset="UTF-8" method="post" action="" @submit="submit" enctype="text/plain" name="contactForm"
+	<form v-if="!loading" accept-charset="UTF-8" method="post" action="" @submit="submit" enctype="text/plain" name="contactForm"
 		autocomplete="on">
 		<label class="label" for="from">From:
 			<input v-model="name" type="text" name="from" id="from" required class="input"
@@ -17,19 +17,35 @@
 				placeholder="I love your website and wanted to know how you built it..."></textarea></label>
 		<input type="submit" value="Send" class="input button" />
 	</form>
+	<loading v-else></loading>
 </template>
 
 <script setup lang="ts">
 import Email from '../models/Email';
 import { reactive, ref } from 'vue';
 import { $ref } from 'vue/macros';
+import router from '@/router';
+import { EmailJSResponseStatus } from '@emailjs/browser';
 
 const name = ref<string>('');
 const email = ref<string>('');
 const msg = ref<string>('');
 const subject = ref<string>('');
 
-const submit = () => Email.sendEmail(email.value, name.value, subject.value, msg.value);
+let loading = ref(false);
+const sentSuccess = (response: EmailJSResponseStatus) => {
+	loading.value = false;
+	router.push(router.resolve('success'));
+};
+const sendFailure = (error: any) => {
+	loading.value = false;
+	console.log(error);
+};
+const submit = () => {
+	loading.value = true;
+	const response = Email.sendEmail(email.value, name.value, subject.value, msg.value);
+	response.then(sentSuccess,sendFailure);
+};
 </script>
 
 <style scoped>
